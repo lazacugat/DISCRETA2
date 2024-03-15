e9d71659f03fec50a19249b7dcd9c081fea7d705
lazacugat <133623179+lazacugat@users.noreply.github.com>
2024-03-15 11:42:50 -0300

    Update funcgraph.h
    Funciones nuevas

--- funcgraph.h
+++ funcgraph.h
@@ -3,9 +3,6 @@
 
 #include "EstructuraGrafo24.h"
 
-// Crear vertice para un grafo
-Vertex create_vertex(u32 v);
-
 // Cantidad de vertices que posee el grafo
 u32 NumeroDeVertices(Grafo G);
 
@@ -15,11 +12,31 @@ u32 NumeroDeLados(Grafo G);
 // Devuelve ∆(G), es decir, el mayor grado del grafo
 u32 Delta(Grafo G);
 
+// Devuelve el grado de un vertice
+u32 Grado(u32 i,Grafo G);
+
+// Devuelve el color de un vertice
+color Color(u32 i,Grafo G);
+
+// Devuelve el j-esimo vecino del vertice i
+u32 Vecino(u32 j,u32 i,Grafo G);
+
+// Da valor al color de un vertice
+void AsignarColor(color x,u32 i,Grafo G);
+
+// Copiar los colores de los vertices de un grafo en un arreglo
+void ExtraerColores(Grafo G,color* Color);
+
+// Copiar los colores de un arreglo a los vertices de un grafo
+void ImportarColores(color* Color,Grafo G);
+
+// Agrega un vertice a un grafo
+Grafo add_vertex(Grafo graph,u32 v);
+
 // Construccion del grafo
 Grafo ConstruirGrafo();
 
 // Liberar memoroia del grafo
 void DestruirGrafo(Grafo G);
-
 
 #endif
